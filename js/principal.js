9b09134c1d4ad607e2301c1aba583988addbc577
felipe-testanalyst <FPEREIRA@tfal.com.au>
2020-03-25 14:32:18 +1100

    aula 3 ex 1

--- js/principal.js
+++ js/principal.js
@@ -1,36 +1,36 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
-console.log(paciente);
+var pacientes = document.querySelectorAll(".paciente"); //busca lista de pacientes pela classe .paciente
+console.log(pacientes);
 
-var tdPeso = document.querySelector(".info-peso");
-var peso = tdPeso.textContent;
-console.log(peso);
+for(var i = 0; i < pacientes.length; i++){ //inicializa o loop
 
-var tdAltura = document.querySelector(".info-altura");
-var altura = tdAltura.textContent;
-console.log(altura);
+    var paciente = pacientes[i]; //define a variavel paciente pare receber o paciente na posicao inicial
 
-var tdImc = document.querySelector(".info-imc");
-console.log(tdImc);
+    var tdPeso = paciente.querySelector(".info-peso");      //busca a informacao de peso do paciente dentro da variavel paciente acima
+    var tdAltura = paciente.querySelector(".info-altura");  //busca a informacao de altura do paciente dentro da variavel paciente acima
+    var tdImc = paciente.querySelector(".info-imc");        //busca a informacao de imc do paciente dentro da variavel paciente acima
 
-var pesoValido = true;
-var alturaValido = true;
+    var peso = tdPeso.textContent;                          // converte o tdPeso para texto e coloca na variavel peso
+    var altura = tdAltura.textContent;                      // converte o tdAl;tura para texto e coloca na variavel altura
 
-if(peso <= 0 || peso >= 1000){
-    pesoValido = false
-    tdImc.textContent = "Peso invalido"
-}
+    var pesoValido = true;                                  //inicializa variavel como falsa para validacao de peso
+    var alturaValido = true;                                //inicializa variavel como falsa para validacao de altura
 
-if(altura <= 0 || altura >= 3.00){
-    alturaValido = false
-    tdImc.textContent = "Altura invalida"
-}
+    if(peso <= 0 || peso >= 1000){                          //validacao
+        pesoValido = false                                  // caso o if seja true muda a variavel para false
+        tdImc.textContent = "Peso invalido"                 //adiciona texto de validacao no tdImc
+    }
 
-if(pesoValido && alturaValido){
-    var imc = peso / (altura*altura);
-    console.log(imc);
+    if(altura <= 0 || altura >= 3.00){
+        alturaValido = false
+        tdImc.textContent = "Altura invalida"
+    }
 
-    tdImc.textContent = imc;
+    if(pesoValido && alturaValido){                         //caso as variaveis de validacao nao mudem para falso
+        var imc = peso / (altura*altura);                   // calcula imc
+        tdImc.textContent = imc.toFixed(2);                //adiciona imc calculado na tdImc para todos os pacientes do loop
+    }
+
 }
