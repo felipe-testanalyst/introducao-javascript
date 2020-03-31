600b47d46803a482e975e5f2c7732cd600bed340
felipe-testanalyst <FPEREIRA@tfal.com.au>
2020-04-01 08:32:09 +1100

    refatorando codigo

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -33,58 +33,13 @@ for(var i = 0; i < pacientes.length; i++){ //inicializa o loop
     }
 
     if(pesoValido && alturaValido){                         //caso as variaveis de validacao nao mudem para falso
-        var imc = peso / (altura*altura);                   // calcula imc
-        tdImc.textContent = imc.toFixed(2);                //adiciona imc calculado na tdImc para todos os pacientes do loop
+        tdImc.textContent = calculaImc(altura ,peso);                //adiciona imc calculado na tdImc para todos os pacientes do loop
     }
 
 }
 
-var botaoAdiciona = document.querySelector("#adicionar-paciente"); //traz o botao do html
+function calculaImc(peso, altura){
+    var imc = peso / (altura*altura); 
+    return imc.toFixed(2);
 
-botaoAdiciona.addEventListener("click", function(event){  //adiciona evento ao botao
-    event.preventDefault();                               // prevem o botao de realizar o comportamento padrao do form
-
-    var form = document.querySelector("#form-adiciona");  // traz o formulario
-
-    //variaveis para captura dos valores do formulario
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.altura.value;
-
-    var pacienteTr = document.createElement("tr");      //Cria a TR (linha) para adicao das celulas (TD)
-
-    //cria as TD (celulas)
-    var nomeTd = document.createElement("td");      
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-
-    //atribui os valores das variaveis para as celulas
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-
-    //anexa as TD a TR
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-
-    var tabela = document.querySelector("#tabela-pacientes"); //traz a tabela pacientes
-
-    //anexa a nova TD a tabela
-    tabela.appendChild(pacienteTr);
-
-    
-
-
-
-
-    
-
-
-
-});
-
+}   
---
+++ js/form.js
@@ -0,0 +1,51 @@
+var botaoAdiciona = document.querySelector("#adicionar-paciente"); //traz o botao do html
+
+botaoAdiciona.addEventListener("click", function(event){  //adiciona evento ao botao
+    event.preventDefault();                               // prevem o botao de realizar o comportamento padrao do form
+
+    var form = document.querySelector("#form-adiciona");  // traz o formulario
+
+    var paciente = obtemPacienteDoForm(form);
+
+    var pacienteTr = montaTr(paciente);
+
+    var tabela = document.querySelector("#tabela-pacientes"); //traz a tabela pacientes
+    tabela.appendChild(pacienteTr);   //anexa a nova TD a tabela
+    form.reset();
+
+});
+
+function obtemPacienteDoForm(form){
+    
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.altura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+    return paciente;
+}
+
+function montaTr(paciente){
+    
+    var pacienteTr = document.createElement("tr");      //Cria a TR (linha) para adicao das celulas (TD)
+    pacienteTr.classList.add("paciente");               //Adiciona classe
+
+    //anexa as TD a TR
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+
+    return pacienteTr;
+}
+
+function montaTd(dado, classe){
+    var td = document.createElement("td");
+    td.classList.add(classe);
+    td.textContent = dado;
+
+    return td;
+}
